1b838fd1db8e4d3ddf6c7f3d441cc23db5a0447c
Mohammad Tariq <ariqxd01@gmail.com>
2025-09-24 01:27:58 +0530

    3 Pages completed

--- script.js
+++ script.js
@@ -40,4 +40,62 @@ function navAnimation() {
     })
 }
 
+function pag2Animate() {
+    let rightElems = document.querySelectorAll(".right-elem")
+let releimg = document.querySelectorAll(".right-elem img")
+
+rightElems.forEach(function (elem) {
+
+    
+    elem.addEventListener("mousemove" , function () {
+        gsap.to(elem.childNodes[3] , {
+            opacity: 1,
+            scale : 1 ,
+
+        })
+        
+    })
+    elem.addEventListener("mouseleave" , function () {
+        gsap.to(elem.childNodes[3] , {
+            opacity: 0 , 
+            scale: 0
+        })
+        
+    })
+    elem.addEventListener("mousemove" , function (e) {
+        gsap.to(elem.childNodes[3] , {
+            x:e.clientX - elem.getBoundingClientRect().x - 50 ,
+            y:e.clientY - elem.getBoundingClientRect().y - 140
+        })
+    })
+})
+}
+
+let page3centre = document.querySelector("#page3 .page3-center")
+let page3video = document.querySelector("video")
+
+page3centre.addEventListener("click" , function () {
+    page3video.play()
+    gsap.to(page3video , {
+        opacity: 1 ,
+        borderRadius:0 , 
+        transform : "scaleX(1) scaleY(1)"
+    })
+})
+
+function page3VideoPlay () {
+    page3video.addEventListener("click", function () {
+        page3video.pause()
+        gsap.to(page3video, {
+            transform: "scaleX(0.5) scaleY(0)",
+            opacity: 0,
+            borderRadius: "30px"
+        })
+    })
+}
+
+page3VideoPlay()
+
 navAnimation()
+
+pag2Animate()
